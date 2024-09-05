d45c6781464d492139794d15e86f647009214d71
Sasi <sasitharani@gmail.com>
2024-09-05 20:18:10 +0530

    data display working

--- src/Body.jsx
+++ src/Body.jsx
@@ -37,11 +37,14 @@ export default function Body() {
             finalBlogData.length >=1 
             ?
             finalBlogData.map((items,index)=>{
+              return (
               <div className='border-2 shadow-gray-600 shadow-lg rounded-xl p-4 m-3'>
               <h1 className="text-xl capitalize font-bold">Demo</h1>
               <p>{items.heading}</p>
               <h1>{items.description}</h1>
               </div>
+            )
+ 
             })
 
               :
